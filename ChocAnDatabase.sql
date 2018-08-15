607518582ef1b83e69f9459df88640ac9c183597
HassanMehdi17 <Hassanmehdi17@gmail.com>
2018-08-15 16:09:16 -0500

    corrected sql grammar

--- ChocAnDatabase.sql
+++ ChocAnDatabase.sql
@@ -1,32 +1,32 @@
---Generated automatically via QuickDBD
-
+use chocoholicsanonymous
+go
 CREATE TABLE [Member] (
-    [MemberNumber] INTEGER(9)  NOT NULL ,
+    [MemberNumber] NUMERIC(9,0)  NOT NULL ,
     [Name] VARCHAR(25)  NOT NULL ,
     [Street] VARCHAR(25)  NOT NULL ,
     [City] VARCHAR(14)  NOT NULL ,
     [State] CHAR(2)  NOT NULL ,
-    [ZipCode] INTEGER(5)  NOT NULL ,
+    [ZipCode] NUMERIC(5)  NOT NULL ,
     [Email] VARCHAR(50)  NOT NULL ,
-    [Status] INTEGER  NOT NULL , -- what data type should this be?
+    [Status] NUMERIC(1,0)  NOT NULL , -- what data type should this be?
     CONSTRAINT [PK_Member]
         PRIMARY KEY CLUSTERED ([MemberNumber] ASC)
 )
 
 CREATE TABLE [Provider] (
-    [ProviderNumber] INTEGER(9)  NOT NULL ,
+    [ProviderNumber] NUMERIC(9, 0)  NOT NULL ,
     [Name] VARCHAR(25)  NOT NULL ,
     [Street] VARCHAR(25)  NOT NULL ,
     [City] VARCHAR(14)  NOT NULL ,
     [State] CHAR(2)  NOT NULL ,
-    [ZipCode] INTEGER(5)  NOT NULL ,
+    [ZipCode] NUMERIC(9, 0)  NOT NULL ,
     [Email] VARCHAR(50)  NOT NULL ,
     CONSTRAINT [PK_Provider]
         PRIMARY KEY CLUSTERED ([ProviderNumber] ASC)
 )
 
 CREATE TABLE [Service] (
-    [ServiceCode] INTEGER(6)  NOT NULL ,
+    [ServiceCode] NUMERIC(6, 0)  NOT NULL ,
     [Name] VARCHAR(20)  NOT NULL ,
     [Fee] DECIMAL(5,2)  NOT NULL ,
     CONSTRAINT [PK_Service] 
@@ -34,39 +34,39 @@ CREATE TABLE [Service] (
 )
 
 CREATE TABLE [ProvidedService] (
-    [MemberNumber] INTEGER(9)  NOT NULL ,
-    [ProviderNumber] INTEGER(9)  NOT NULL ,
-    [ServiceCode] INTEGER(6)  NOT NULL ,
+    [MemberNumber] NUMERIC(9,0)  NOT NULL ,
+    [ProviderNumber] NUMERIC(9,0)  NOT NULL ,
+    [ServiceCode] NUMERIC(6,0)  NOT NULL ,
     [DateProvided] DATE  NOT NULL ,
     [DateReceived] TIMESTAMP  NOT NULL ,
-    [Comment] VARCHAR(100) 
+    [Comment] VARCHAR(100),
     CONSTRAINT [FK_ProvidedService_MemberNumber]
-        FOREIGN KEY([MemberNumber]) REFERENCES [Member] ([MemberNumber])
+        FOREIGN KEY([MemberNumber]) REFERENCES [Member] ([MemberNumber]),
     CONSTRAINT [FK_ProvidedService_ProviderNumber]
-        FOREIGN KEY([ProviderNumber]) REFERENCES [Provider] ([ProviderNumber])
+        FOREIGN KEY([ProviderNumber]) REFERENCES [Provider] ([ProviderNumber]),
     CONSTRAINT [FK_ProvidedService_ServiceCode]
         FOREIGN KEY([ServiceCode]) REFERENCES [Service] ([ServiceCode])
 )
 
 CREATE TABLE [Transaction] (
-    [TransactionNumber] INTEGER(9)  NOT NULL ,
-    [MemberNumber] INTEGER(9)  NOT NULL ,
+    [TransactionNumber] NUMERIC(9, 0)  NOT NULL ,
+    [MemberNumber] NUMERIC(9, 0)  NOT NULL ,
     [Amount] DECIMAL(5,2)  NOT NULL ,
     [Date] TIMESTAMP  NOT NULL ,
     CONSTRAINT [PK_Transaction] 
-        PRIMARY KEY CLUSTERED ([TransactionNumber] ASC)
+        PRIMARY KEY CLUSTERED ([TransactionNumber] ASC),
     CONSTRAINT [FK_Transaction_MemberNumber]
         FOREIGN KEY([MemberNumber]) REFERENCES [Member] ([MemberNumber]) 
 )
 
 CREATE TABLE [Employee] (
-    [EmployeeNumber] INTEGER(9)  NOT NULL ,
+    [EmployeeNumber] NUMERIC(9, 0)  NOT NULL ,
     [Name] VARCHAR(25)  NOT NULL ,
-    [PhoneNumber] INTEGER(10)  NOT NULL ,
+    [PhoneNumber] NUMERIC(10, 0)  NOT NULL ,
     [Street] VARCHAR(25)  NOT NULL ,
     [City] VARCHAR(14)  NOT NULL ,
     [State] CHAR(2)  NOT NULL ,
-    [ZipCode] INTEGER(5)  NOT NULL ,
+    [ZipCode] NUMERIC(5, 0)  NOT NULL ,
     [Email] VARCHAR(50)  NOT NULL ,
     CONSTRAINT [PK_Employee]
         PRIMARY KEY CLUSTERED ([EmployeeNumber] ASC)
