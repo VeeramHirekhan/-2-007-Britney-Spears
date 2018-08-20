5f13213846b150d54daa3fbd96f1fbc719ac0d85
hexagonhexagon <hexagonhexagon163@gmail.com>
2018-08-19 22:29:42 -0400

    Most functions implemented, errors corrected

--- modifydatabase.js
+++ modifydatabase.js
@@ -14,13 +14,13 @@ var config = {
 var connection = new Connection(config);
 
 function addMember(memberNumber, name, address, email, status) {
-  var splitAddress = str.split(', ');
+  var splitAddress = address.split(', ');
   var street = splitAddress[0];
   var city = splitAddress[1];
   var state = splitAddress[2];
   var zipCode = splitAddress[3];
   request = new Request(
-    'INSERT INTO Member (MemberNumber, Name, Street, City, State, ZipCode, Email, Status)',
+    'INSERT INTO Member (MemberNumber, Name, Street, City, State, ZipCode, Email, Status) VALUES(@MemberNumber, @Name, @Street, @City, @State, @ZipCode, @Email, @Status);',
     function(err, rowCount, rows) {
       if (err) console.log(err);
       console.log(rowCount + " row(s) written.")
@@ -39,13 +39,13 @@ function addMember(memberNumber, name, address, email, status) {
 }
 
 function addProvider(providerNumber, name, address, email) {
-  var splitAddress = str.split(', ');
+  var splitAddress = address.split(', ');
   var street = splitAddress[0];
   var city = splitAddress[1];
   var state = splitAddress[2];
   var zipCode = splitAddress[3];
   request = new Request(
-    'INSERT INTO Member (ProviderNumber, Name, Street, City, State, ZipCode, Email)',
+    'INSERT INTO Provider (ProviderNumber, Name, Street, City, State, ZipCode, Email) VALUES(@ProviderNumber, @Name, @Street, @City, @State, @ZipCode, @Email);',
     function(err, rowCount, rows) {
       if (err) console.log(err);
       console.log(rowCount + " row(s) written.")
@@ -63,5 +63,44 @@ function addProvider(providerNumber, name, address, email) {
 }
 
 function addProvidedService(memberNumber, providerNumber, serviceCode, dateProvided, comment) {
+  request = new Request(
+    'INSERT INTO ProvidedService (MemberNumber, ProviderNumber, ServiceCode, DateProvided, DateReceived, Comment) VALUES (@MemberNumber, @ProviderNumber, @ServiceCode, @DateProvided, CURRENT_TIMESTAMP, @Comment);',
+    function(err, rowCount, rows) {
+      if (err) console.log(err);
+      console.log(rowCount + " row(s) written.")
+    }
+  );
+  request.addParameter('MemberNumber', TYPES.Numeric, memberNumber);
+  request.addParameter('ProviderNumber', TYPES.Numeric, providerNumber);
+  request.addParameter('ServiceCode', TYPES.Numeric, serviceCode);
+  request.addParameter('DateProvided', TYPES.Date, dateProvided);
+  request.addParameter('Comment', TYPES.NVarChar, comment);
   
+  connection.execSql(request);
+}
+
+function removeMember(memberNumber) {
+  request = new Request(
+    'DELETE FROM Member WHERE MemberNumber=@MemberNumber;',
+    function(err, rowCount, rows) {
+      if (err) console.log(err);
+      console.log(rowCount + " row(s) removed.")
+    }
+  );
+  request.addParameter('MemberNumber', TYPES.Numeric, memberNumber);
+  
+  connection.execSql(request);
+}
+
+function removeProvider(providerNumber) {
+  request = new Request(
+    'DELETE FROM Provider WHERE ProviderNumber=@ProviderNumber;',
+    function(err, rowCount, rows) {
+      if (err) console.log(err);
+      console.log(rowCount + " row(s) written.")
+    }
+  );
+  request.addParameter('ProviderNumber', TYPES.Numeric, providerNumber);
+  
+  connection.execSql(request);
 }
